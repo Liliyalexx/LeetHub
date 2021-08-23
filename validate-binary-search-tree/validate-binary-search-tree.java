a31422c30abbf8208efcf9bfe1d02541a25fad2c
Liliyalexx <liliya.fedyurina@gmail.com>
2021-08-22 18:08:59 -0700

    Time: 0 ms (100.00%), Space: 38.9 MB (28.07%) - LeetHub

--- validate-binary-search-tree/validate-binary-search-tree.java
+++ validate-binary-search-tree/validate-binary-search-tree.java
@@ -1,37 +1,42 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public boolean isValidBST(TreeNode root) {
-        Stack<TreeNode> stack = new Stack();
-        double left_child_val = -Double.MAX_VALUE;
+
+// class Solution {
+//     public boolean isValidBST(TreeNode root) {
+//         Stack<TreeNode> stack = new Stack();
+//         double left_child_val = -Double.MAX_VALUE;
         
-        while (!stack.isEmpty() || root !=null){
-           while(root !=null){
-            stack.push(root);
-               root = root.left;
-           } 
+//         while (!stack.isEmpty() || root !=null){
+//            while(root !=null){
+//             stack.push(root);
+//                root = root.left;
+//            } 
             
             
-            root = stack.pop();
-            if(root.val <= left_child_val) return false;
-            left_child_val = root.val;
-            root = root.right;
+//             root = stack.pop();
+//             if(root.val <= left_child_val) return false;
+//             left_child_val = root.val;
+//             root = root.right;
             
-        }
+//         }
         
-        return true;
-    }
+//         return true;
+//     }
+// }
+
+class Solution{
+public boolean isValidBST(TreeNode root){
+return validate(root, null, null);
 }
+    
+ public boolean validate(TreeNode root, Integer max, Integer min){
+    if(root ==null){
+        return true;
+    } else if(max !=null && root.val>=max || min!= null && root.val <= min){
+       return false;
+    } else {
+        return validate(root.left, root.val, min) && validate(root.right, max, root.val);
+    }
+ }   
+    
+}
+// Space Complexity: O(n);
+// Run Time: O(n);
