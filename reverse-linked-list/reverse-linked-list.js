f2d369d062334ffb349dce9d27a672d3f3061054
Liliyalexx <liliya.fedyurina@gmail.com>
2021-09-23 10:02:02 -0700

    Time: 76 ms (81.31%), Space: 41.2 MB (6.27%) - LeetHub

---
+++ reverse-linked-list/reverse-linked-list.js
@@ -0,0 +1,18 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var reverseList = function(head) {
+    let [prev, cur] = [null, head]
+    while(cur) {
+        [cur.next, prev, cur] = [prev, cur, cur.next]
+    }
+    return prev
+}
