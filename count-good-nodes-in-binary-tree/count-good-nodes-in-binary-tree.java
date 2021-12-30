00807d7e44060b5680b86c006f8d3e753f053b7c
Liliyalexx <liliya.fedyurina@gmail.com>
2021-12-30 07:01:02 -0800

    Time: 17 ms (5.01%), Space: 59.4 MB (5.21%) - LeetHub

--- count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
+++ count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.java
@@ -13,35 +13,68 @@
  *     }
  * }
  */
-class Solution { 
+// class Solution { 
     
-    private int numOfGoodNodes = 0;
+//     private int numOfGoodNodes = 0;
     
-    public int goodNodes(TreeNode root) {
-       dfs(root, Integer.MIN_VALUE);
+//     public int goodNodes(TreeNode root) {
+//        dfs(root, Integer.MIN_VALUE);
         
-        return numOfGoodNodes;
-    }
+//         return numOfGoodNodes;
+//     }
     
-    private void dfs(TreeNode node, int maxSoFar){
+//     private void dfs(TreeNode node, int maxSoFar){
               
-        if (maxSoFar <= node.val) { 
-            numOfGoodNodes++;
+//         if (maxSoFar <= node.val) { 
+//             numOfGoodNodes++;
             
-            }
+//             }
         
-         if (node.right != null) {
-            dfs(node.right, Math.max(node.val, maxSoFar));
-        }
+//          if (node.right != null) {
+//             dfs(node.right, Math.max(node.val, maxSoFar));
+//         }
         
-        if (node.left != null) {
-            dfs(node.left, Math.max(node.val, maxSoFar));
-        }
+//         if (node.left != null) {
+//             dfs(node.left, Math.max(node.val, maxSoFar));
+//         }
             
             
-    }
+//     }
         
                            
-}
+// }
         
-       
+     class Pair {
+    public TreeNode node;
+    public int maxSoFar;
+    
+    public Pair(TreeNode node, int maxSoFar) {
+        this.node = node;
+        this.maxSoFar = maxSoFar;
+    }
+}
+
+class Solution {
+    public int goodNodes(TreeNode root) {
+        int numGoodNodes = 0;
+        Stack<Pair> stack = new Stack<>();
+        stack.push(new Pair(root, Integer.MIN_VALUE));
+        
+        while (stack.size() > 0) {
+            Pair curr = stack.pop();
+            if (curr.maxSoFar <= curr.node.val) {
+                numGoodNodes++;
+            }
+            
+            if (curr.node.left != null) {
+                stack.push(new Pair(curr.node.left, Math.max(curr.node.val, curr.maxSoFar)));
+            }
+            
+            if (curr.node.right != null) {
+                stack.push(new Pair(curr.node.right, Math.max(curr.node.val, curr.maxSoFar)));
+            }
+        }
+
+        return numGoodNodes;
+    }
+}  
