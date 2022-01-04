b91b3761c83ca99bf45c7c529df5bb204fd07b3f
Liliyalexx <liliya.fedyurina@gmail.com>
2022-01-04 12:55:41 -0800

    Time: 1 ms (36.80%), Space: 39.5 MB (18.62%) - LeetHub

--- 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
+++ 114-flatten-binary-tree-to-linked-list/114-flatten-binary-tree-to-linked-list.java
@@ -13,35 +13,76 @@
  *     }
  * }
  */
-class Solution {
-    public void flatten(TreeNode root) {
-        if( root == null) return;
+// class Solution {
+//     public void flatten(TreeNode root) {
+//         if( root == null) return;
         
-        Stack<TreeNode> stack = new Stack();
-        stack.push(root);
+//         Stack<TreeNode> stack = new Stack();
+//         stack.push(root);
         
         
-        while (!stack.isEmpty()){
-            TreeNode current_node = stack.pop();
-            if (current_node.right !=null) {
-              stack.push(current_node.right);
+//         while (!stack.isEmpty()){
+//             TreeNode current_node = stack.pop();
+//             if (current_node.right !=null) {
+//               stack.push(current_node.right);
                 
             
-            }
+//             }
             
-            if(current_node.left !=null){
-               stack.push(current_node.left);
+//             if(current_node.left !=null){
+//                stack.push(current_node.left);
             
-            }
+//             }
             
-            if(!stack.isEmpty()) {
-            current_node.right = stack.peek();
+//             if(!stack.isEmpty()) {
+//             current_node.right = stack.peek();
             
-            }
+//             }
             
-            current_node.left = null;
+//             current_node.left = null;
+//         }
+        
+        
+//     }
+// }
+
+class Solution {
+    
+    private TreeNode flattenTree(TreeNode node) {
+        
+        // Handle the null scenario
+        if (node == null) {
+            return null;
+        }
+            
+        // For a leaf node, we simply return the
+        // node as is.
+        if (node.left == null && node.right == null) {
+            return node;
         }
         
+        //Recursively flatten the left subtree
+        TreeNode leftTail = this.flattenTree(node.left);
         
+        // Recursively flatten the right subtree
+        TreeNode rightTail = this.flattenTree(node.right);
+        
+        // If there was a left subtree, we shuffle the connections
+        // around so that there is nothing on the left side
+        // anymore.
+        if (leftTail != null) {
+            leftTail.right = node.right;
+            node.right = node.left;
+            node.left = null;
+        }
+        
+        // We need to return the "rightmost" node after we are
+        // done wiring the new connections. 
+        return rightTail == null ? leftTail : rightTail;
+    }
+    
+    public void flatten(TreeNode root) {
+        
+        this.flattenTree(root);
     }
 }
