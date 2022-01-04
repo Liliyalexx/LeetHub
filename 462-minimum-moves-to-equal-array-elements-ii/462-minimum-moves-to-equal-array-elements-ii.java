f015576cc727d6ae143200c4e3e0e80e925e0de8
Liliyalexx <liliya.fedyurina@gmail.com>
2022-01-03 21:58:06 -0800

    Time: 75 ms (13.04%), Space: 40.1 MB (36.05%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -1,12 +1,29 @@
+// public class Solution {
+//     public int minMoves2(int[] nums) {
+//         long min = Integer.MAX_VALUE;
+//         for (int num : nums) {
+//             long sum = 0;
+//             for (int n : nums) {
+//                 sum += Math.abs(n - num);
+//             }
+//             min = Math.min(min, sum);
+//         }
+//         return (int) min;
+//     }
+// }
+
 public class Solution {
     public int minMoves2(int[] nums) {
-        long min = Integer.MAX_VALUE;
+        Arrays.sort(nums);
+        long min = Long.MAX_VALUE, sum = 0, total = 0;
         for (int num : nums) {
-            long sum = 0;
-            for (int n : nums) {
-                sum += Math.abs(n - num);
-            }
-            min = Math.min(min, sum);
+            total += num;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            long ans = ((long) nums[i] * i - sum) + ((total - sum) - (long) nums[i] * (nums.length - i));
+            System.out.println(nums[i] + " " + ans);
+            min = Math.min(min, ans);
+            sum += nums[i];
         }
         return (int) min;
     }
