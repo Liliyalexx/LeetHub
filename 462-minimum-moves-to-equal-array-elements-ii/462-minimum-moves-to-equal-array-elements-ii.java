dddd907ddf3e3b8de73d2a25030700d0650ef67e
Liliyalexx <liliya.fedyurina@gmail.com>
2022-01-03 22:03:06 -0800

    Time: 2 ms (99.21%), Space: 39.8 MB (51.53%) - LeetHub

--- 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
+++ 462-minimum-moves-to-equal-array-elements-ii/462-minimum-moves-to-equal-array-elements-ii.java
@@ -12,19 +12,30 @@
 //     }
 // }
 
+// public class Solution {
+//     public int minMoves2(int[] nums) {
+//         Arrays.sort(nums);
+//         long min = Long.MAX_VALUE, sum = 0, total = 0;
+//         for (int num : nums) {
+//             total += num;
+//         }
+//         for (int i = 0; i < nums.length; i++) {
+//             long ans = ((long) nums[i] * i - sum) + ((total - sum) - (long) nums[i] * (nums.length - i));
+//             System.out.println(nums[i] + " " + ans);
+//             min = Math.min(min, ans);
+//             sum += nums[i];
+//         }
+//         return (int) min;
+//     }
+// }
+
 public class Solution {
     public int minMoves2(int[] nums) {
         Arrays.sort(nums);
-        long min = Long.MAX_VALUE, sum = 0, total = 0;
+        int sum = 0;
         for (int num : nums) {
-            total += num;
+            sum += Math.abs(nums[nums.length / 2] - num);
         }
-        for (int i = 0; i < nums.length; i++) {
-            long ans = ((long) nums[i] * i - sum) + ((total - sum) - (long) nums[i] * (nums.length - i));
-            System.out.println(nums[i] + " " + ans);
-            min = Math.min(min, ans);
-            sum += nums[i];
-        }
-        return (int) min;
+        return sum;
     }
 }
