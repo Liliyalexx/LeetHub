d632e5c61211d087ef59c790415adaae0b92f64a
Liliyalexx <liliya.fedyurina@gmail.com>
2022-01-08 19:19:19 -0800

    Time: 11 ms (28.48%), Space: 115.4 MB (25.04%) - LeetHub

--- 234-palindrome-linked-list/234-palindrome-linked-list.java
+++ 234-palindrome-linked-list/234-palindrome-linked-list.java
@@ -8,35 +8,87 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-class Solution {
-    public boolean isPalindrome(ListNode head) {
-       List<Integer> list = new ArrayList<>();
+// class Solution {
+//     public boolean isPalindrome(ListNode head) {
+//        List<Integer> list = new ArrayList<>();
         
-        //Convert LInked List into ArrayList
-        ListNode currentNode = head;
-        while (currentNode != null) {
-            list.add(currentNode.val);
-            currentNode = currentNode.next;
+//         //Convert LInked List into ArrayList
+//         ListNode currentNode = head;
+//         while (currentNode != null) {
+//             list.add(currentNode.val);
+//             currentNode = currentNode.next;
             
-        }
+//         }
         
-        //Reverse list ArrayList
+//         //Reverse list ArrayList
         
-        List<Integer> reversedList = new ArrayList<>();
+//         List<Integer> reversedList = new ArrayList<>();
         
-        for(int i = list.size() -1; i >=0; i--){
-            reversedList.add(list.get(i));
-        }
-        //Check if the two array list are equal to each other
+//         for(int i = list.size() -1; i >=0; i--){
+//             reversedList.add(list.get(i));
+//         }
+//         //Check if the two array list are equal to each other
         
-          for(int i =0; i < list.size(); i++){
-                if(list.get(i) != reversedList.get(i)) {
-                return false; 
-                    }
-          }
+//           for(int i =0; i < list.size(); i++){
+//                 if(list.get(i) != reversedList.get(i)) {
+//                 return false; 
+//                     }
+//           }
     
-                return true;
+//                 return true;
 
         
+//     }
+// }
+
+//Time O(3N) = O(N)
+//Space O(2N) = O(N)
+
+class Solution {
+
+    public boolean isPalindrome(ListNode head) {
+
+        if (head == null) return true;
+
+        // Find the end of first half and reverse second half.
+        ListNode firstHalfEnd = endOfFirstHalf(head);
+        ListNode secondHalfStart = reverseList(firstHalfEnd.next);
+
+        // Check whether or not there is a palindrome.
+        ListNode p1 = head;
+        ListNode p2 = secondHalfStart;
+        boolean result = true;
+        while (result && p2 != null) {
+            if (p1.val != p2.val) result = false;
+            p1 = p1.next;
+            p2 = p2.next;
+        }        
+
+        // Restore the list and return the result.
+        firstHalfEnd.next = reverseList(secondHalfStart);
+        return result;
+    }
+
+    // Taken from https://leetcode.com/problems/reverse-linked-list/solution/
+    private ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+        ListNode curr = head;
+        while (curr != null) {
+            ListNode nextTemp = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = nextTemp;
+        }
+        return prev;
+    }
+
+    private ListNode endOfFirstHalf(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        while (fast.next != null && fast.next.next != null) {
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        return slow;
     }
 }
