1bc0e94630891a3b987a3ec9a2f3514db9e1bfc6
Liliyalexx <liliya.fedyurina@gmail.com>
2022-01-10 11:07:57 -0800

    Time: 8 ms (49.64%), Space: 38.9 MB (41.38%) - LeetHub

--- 647-palindromic-substrings/647-palindromic-substrings.java
+++ 647-palindromic-substrings/647-palindromic-substrings.java
@@ -1,3 +1,66 @@
+// class Solution {
+//     public int countSubstrings(String s) {
+//         int n = s.length(), ans = 0;
+
+//         if (n <= 0) 
+//             return 0;
+
+//         boolean[][] dp = new boolean[n][n];
+
+//         // Base case: single letter substrings
+//         for (int i = 0; i < n; ++i, ++ans) 
+//             dp[i][i] = true;
+
+//         // Base case: double letter substrings
+//         for (int i = 0; i < n - 1; ++i) {
+//             dp[i][i + 1] = (s.charAt(i) == s.charAt(i + 1));
+//             ans += (dp[i][i + 1] ? 1 : 0);
+//         }
+
+//         // All other cases: substrings of length 3 to n
+//         for (int len = 3; len <= n; ++len)
+//             for (int i = 0, j = i + len - 1; j < n; ++i, ++j) {
+//                 dp[i][j] = dp[i + 1][j - 1] && (s.charAt(i) == s.charAt(j));
+//                 ans += (dp[i][j] ? 1 : 0);
+//             }
+
+//         return ans;
+//     }
+// }
+
+// class Solution {
+//     public int countSubstrings(String s) {
+//         int ans = 0;
+
+//         for (int i = 0; i < s.length(); ++i) {
+//             // odd-length palindromes, single character center
+//             ans += countPalindromesAroundCenter(s, i, i);
+
+//             // even-length palindromes, consecutive characters center
+//             ans += countPalindromesAroundCenter(s, i, i + 1);
+//         }
+
+//         return ans;
+//     }
+
+//     private int countPalindromesAroundCenter(String ss, int lo, int hi) {
+//         int ans = 0;
+
+//         while (lo >= 0 && hi < ss.length()) {
+//             if (ss.charAt(lo) != ss.charAt(hi))
+//                 break;      // the first and last characters don't match!
+
+//             // expand around the center
+//             lo--;
+//             hi++;
+
+//             ans++;
+//         }
+
+//         return ans;
+//     }
+// }
+
 class Solution {
     public int countSubstrings(String s) {
         int n = s.length(), ans = 0;
@@ -27,3 +90,4 @@ class Solution {
         return ans;
     }
 }
+
