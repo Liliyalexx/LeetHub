50b50b01fb5f71448d94f374cc17d6ec777bf78c
Liliyalexx <liliya.fedyurina@gmail.com>
2022-01-12 13:56:52 -0800

    Time: 14 ms (10.49%), Space: 42.2 MB (20.07%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.java
+++ 252-meeting-rooms/252-meeting-rooms.java
@@ -1,17 +1,29 @@
+// class Solution {
+//     public boolean canAttendMeetings(int[][] intervals) {
+//         for (int i = 0; i < intervals.length; i++) {
+//             for (int j = i + 1; j < intervals.length; j++) {
+//                 if (overlap(intervals[i], intervals[j])) {
+//                     return false;
+//                 }
+//             }
+//         }
+//         return true;
+//     }
+
+//     private boolean overlap(int[] interval1, int[] interval2) {
+//         return (interval1[0] >= interval2[0] && interval1[0] < interval2[1])
+//             || (interval2[0] >= interval1[0] && interval2[0] < interval1[1]);
+//     }
+// }
+
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
-        for (int i = 0; i < intervals.length; i++) {
-            for (int j = i + 1; j < intervals.length; j++) {
-                if (overlap(intervals[i], intervals[j])) {
-                    return false;
-                }
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        for (int i = 0; i < intervals.length - 1; i++) {
+            if (intervals[i][1] > intervals[i + 1][0]) {
+                return false;
             }
         }
         return true;
-    }
-
-    private boolean overlap(int[] interval1, int[] interval2) {
-        return (interval1[0] >= interval2[0] && interval1[0] < interval2[1])
-            || (interval2[0] >= interval1[0] && interval2[0] < interval1[1]);
     }
 }
