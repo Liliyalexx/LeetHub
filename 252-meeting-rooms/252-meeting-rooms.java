874ea0c05addfcc88de59dd7412eae54844949ae
Liliyalexx <liliya.fedyurina@gmail.com>
2022-01-14 18:48:04 -0800

    Time: 4 ms (97.56%), Space: 38.9 MB (58.81%) - LeetHub

--- 252-meeting-rooms/252-meeting-rooms.java
+++ 252-meeting-rooms/252-meeting-rooms.java
@@ -1,21 +1,3 @@
-// class Solution {
-//     public boolean canAttendMeetings(int[][] intervals) {
-//         for (int i = 0; i < intervals.length; i++) {
-//             for (int j = i + 1; j < intervals.length; j++) {
-//                 if (overlap(intervals[i], intervals[j])) {
-//                     return false;
-//                 }
-//             }
-//         }
-//         return true;
-//     }
-
-//     private boolean overlap(int[] interval1, int[] interval2) {
-//         return (interval1[0] >= interval2[0] && interval1[0] < interval2[1])
-//             || (interval2[0] >= interval1[0] && interval2[0] < interval1[1]);
-//     }
-// }
-
 class Solution {
     public boolean canAttendMeetings(int[][] intervals) {
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
@@ -27,5 +9,3 @@ class Solution {
         return true;
     }
 }
-
-
