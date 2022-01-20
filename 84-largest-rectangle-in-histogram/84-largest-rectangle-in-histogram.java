6d31024c4d7dc051225a1f84535cc87ff13590f3
Liliyalexx <liliya.fedyurina@gmail.com>
2022-01-19 19:41:40 -0800

    Time: 48 ms (38.13%), Space: 48.4 MB (85.56%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.java
@@ -1,23 +1,68 @@
-public class Solution {
+// public class Solution {
+//     public int largestRectangleArea(int[] heights) {
+//         Deque<Integer> stack = new ArrayDeque<>();
+//         stack.push(-1);
+//         int length = heights.length;
+//         int maxArea = 0;
+//         for (int i = 0; i < length; i++) {
+//             while ((stack.peek() != -1)
+//                     && (heights[stack.peek()] >= heights[i])) {
+//                 int currentHeight = heights[stack.pop()];
+//                 int currentWidth = i - stack.peek() - 1;
+//                 maxArea = Math.max(maxArea, currentHeight * currentWidth);
+//             }
+//             stack.push(i);
+//         }
+//         while (stack.peek() != -1) {
+//             int currentHeight = heights[stack.pop()];
+//             int currentWidth = length - stack.peek() - 1;
+//             maxArea = Math.max(maxArea, currentHeight * currentWidth);
+//         }
+//         return maxArea;
+//     }
+// }
+
+
+class Solution {
     public int largestRectangleArea(int[] heights) {
-        Deque<Integer> stack = new ArrayDeque<>();
-        stack.push(-1);
-        int length = heights.length;
-        int maxArea = 0;
-        for (int i = 0; i < length; i++) {
-            while ((stack.peek() != -1)
-                    && (heights[stack.peek()] >= heights[i])) {
-                int currentHeight = heights[stack.pop()];
-                int currentWidth = i - stack.peek() - 1;
-                maxArea = Math.max(maxArea, currentHeight * currentWidth);
+
+        int arraySize =  heights.length;
+        int[] left = new int[arraySize];
+        int[] right = new int[arraySize];
+        Stack<Integer> stack = new Stack<>();
+
+        for (int i =0; i < arraySize; i++) {
+            if (stack.isEmpty()) {
+                left[i] = 0;
+            } else {
+                while (!stack.empty() && heights[stack.peek()] >= heights[i]) {
+                    stack.pop();
+                }
+                left[i] = stack.empty() ? 0 : stack.peek() + 1;
             }
             stack.push(i);
         }
-        while (stack.peek() != -1) {
-            int currentHeight = heights[stack.pop()];
-            int currentWidth = length - stack.peek() - 1;
-            maxArea = Math.max(maxArea, currentHeight * currentWidth);
+
+        stack.clear();
+
+        for (int i = arraySize-1; i >= 0; i--) {
+            if (stack.empty()) {
+                right[i] = arraySize - 1;
+            } else {
+                while (!stack.isEmpty() && heights[stack.peek()] >= heights[i]){
+                    stack.pop();
+                }
+                right[i] = stack.isEmpty() ? arraySize - 1 : stack.peek() - 1;
+            }
+            stack.push(i);
+        }
+
+        int maxArea = 0;
+        for (int i =0; i < arraySize; i++) {
+            int area = heights[i] * (right[i] - left[i] + 1);
+            maxArea = Math.max(maxArea, area);
         }
         return maxArea;
     }
+
 }
