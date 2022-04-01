f313a1c2ab79e5d2fce05ac696cdf176b947c31d
Liliyalexx <liliya.fedyurina@gmail.com>
2022-04-01 09:50:44 -0700

    Time: 0 ms (100.00%), Space: 42.9 MB (7.43%) - LeetHub

--- 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.java
+++ 1304-find-n-unique-integers-sum-up-to-zero/1304-find-n-unique-integers-sum-up-to-zero.java
@@ -1,10 +1,22 @@
+// class Solution {
+//     public int[] sumZero(int n) {
+//         int[] result = new int[n];
+//         for (int i = 0; i < n - 1; i += 2) {
+//             result[i] = i + 1;
+//             result[i + 1] = -(i + 1);
+//         }
+//         return result;
+//     }
+// }
+
 class Solution {
     public int[] sumZero(int n) {
-        int[] result = new int[n];
-        for (int i = 0; i < n - 1; i += 2) {
-            result[i] = i + 1;
-            result[i + 1] = -(i + 1);
+        
+        
+        int[] ans = new int[n];
+        for(int i=0; i<n; i++) {
+            ans[i]  = (i*2)-n+1;
         }
-        return result;
+        return ans;
     }
 }
