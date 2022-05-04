ce050a3cde1b458b3b571512cbbd5c643629ad80
Liliyalexx <liliya.fedyurina@gmail.com>
2022-05-04 10:17:45 -0700

    Time: 2 ms (29.79%), Space: 56.1 MB (33.76%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -1,24 +1,24 @@
-// class Solution {
-//   public void helper(char[] s, int left, int right) {
-//     if (left >= right) return;
-//     char tmp = s[left];
-//     s[left++] = s[right];
-//     s[right--] = tmp;
-//     helper(s, left, right);
-//   }
-
-//   public void reverseString(char[] s) {
-//     helper(s, 0, s.length - 1);
-//   }
-// }
-
 class Solution {
-    public void reverseString(char[] s) {
-        int left = 0, right = s.length - 1;
-        while (left < right) {
-            char tmp = s[left];
-            s[left++] = s[right];
-            s[right--] = tmp;
-        }
-    }
+  public void helper(char[] s, int left, int right) {
+    if (left >= right) return;
+    char tmp = s[left];
+    s[left++] = s[right];
+    s[right--] = tmp;
+    helper(s, left, right);
+  }
+
+  public void reverseString(char[] s) {
+    helper(s, 0, s.length - 1);
+  }
 }
+
+// class Solution {
+//     public void reverseString(char[] s) {
+//         int left = 0, right = s.length - 1;
+//         while (left < right) {
+//             char tmp = s[left];
+//             s[left++] = s[right];
+//             s[right--] = tmp;
+//         }
+//     }
+// }
