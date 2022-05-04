e53575e770ec34332db22e7b0a7581e775159c28
Liliyalexx <liliya.fedyurina@gmail.com>
2022-05-04 10:12:08 -0700

    Time: 1 ms (83.89%), Space: 54.3 MB (64.46%) - LeetHub

--- 344-reverse-string/344-reverse-string.java
+++ 344-reverse-string/344-reverse-string.java
@@ -1,3 +1,17 @@
+// class Solution {
+//   public void helper(char[] s, int left, int right) {
+//     if (left >= right) return;
+//     char tmp = s[left];
+//     s[left++] = s[right];
+//     s[right--] = tmp;
+//     helper(s, left, right);
+//   }
+
+//   public void reverseString(char[] s) {
+//     helper(s, 0, s.length - 1);
+//   }
+// }
+
 class Solution {
     public void reverseString(char[] s) {
         int left = 0, right = s.length - 1;
