1af9477058c2f73161a0bca0a7d180ba5ac27208
Liliyalexx <liliya.fedyurina@gmail.com>
2022-06-09 13:58:42 -0700

    Time: 8 ms (72.41%), Space: 45.1 MB (41.38%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.java
@@ -1,46 +1,22 @@
-// class Solution {
-//     public int lengthOfLongestSubstring(String s) {
-//         Integer[] chars = new Integer[128];
-        
-//         int left = 0;
-//         int right = 0;
-        
-//         int res = 0;
-        
-//         while(right < s.length()){
-//            char r = s.charAt(right);
-//             char[r]++;
-            
-//             Integer index = chars[r];
-            
-//             if(index !=null && index >= left && index < right ) left = index + 1;
-                    
-//             res = Math.max(res, right - left +1);
-            
-//             chars[r] = right;
-            
-//             right ++;
-//             }
-        
-//         return res;
-//     }
-// }
-
-//Time: O(n)
-//Space: O(m)
-
-public class Solution {
+class Solution {
     public int lengthOfLongestSubstring(String s) {
-        int n = s.length(), ans = 0;
-        Map<Character, Integer> map = new HashMap<>(); // current index of character
-        // try to extend the range [i, j]
-        for (int j = 0, i = 0; j < n; j++) {
-            if (map.containsKey(s.charAt(j))) {
-                i = Math.max(map.get(s.charAt(j)), i);
-            }
-            ans = Math.max(ans, j - i + 1);
-            map.put(s.charAt(j), j + 1);
+        int i = 0;
+        int j = 0; 
+        int max = 0;
+        
+        HashMap<Character, Integer> map = new HashMap<>();
+        
+        for( i = 0; i<s.length(); i++ ){
+                if(map.containsKey(s.charAt(i))){
+                    j = Math.max(j, map.get(s.charAt(i))+1);
+                }
+                    map.put(s.charAt(i), i);
+        
+            max=Math.max(max, i-j+1);
         }
-        return ans;
+        
+        return max;
+        
+        
     }
 }
